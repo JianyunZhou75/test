0f4a0928c9171fac2042e79d067720789cb2b3af
Jianyun <jzh@statoil.com>
2015-10-19 13:01:57 +0200

    express

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app= express()
-var port = process.env.PORT||3500;
+var port = (process.env.PORT || '3000')
 app.get('/', function(req, res) {
   res.send('Hello World\n')
 })
 app.listen(port)
 
-console.log("My first node application is running on Port"+ port);
+console.log("My first node application is running on Port "+ port);
