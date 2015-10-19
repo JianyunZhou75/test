75b705479000153f79a5b8d50dfb281f3aa3023d
Jianyun <jzh@statoil.com>
2015-10-19 12:31:13 +0200

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app= express()
-var port = process.env.PORT||3000;
+var port = process.env.PORT||3500;
 app.get('/', function(req, res) {
   res.send('Hello World\n')
 })
